37556206cb4271c4b48c6056d1cda2e4b08e9736
Dima <kucennko11@gmai.com>
2023-05-17 03:59:40 +0300

    finis end work

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Отримання посилань на HTML-елементи
 const buttongen = document.getElementById('buttongen');
 const img = document.getElementById('code');
 const text = document.getElementById('text');
@@ -8,36 +9,46 @@ const sky = document.getElementById('sky');
 const purple = document.getElementById('purple');
 const green = document.getElementById('green');
 const reset = document.getElementById('reset');
-let color;
 
+let color; // Змінна для зберігання обраного кольору
+
+// Обробник події для кнопки скидання
 reset.onclick = function () {
-text.value='';
-text.blur();
-img.src='start.png';
-}
+  text.value = ''; // Очистити вміст текстового вводу
+  text.blur(); // Зняти фокус з текстового вводу
+  img.src = 'start.png'; // Встановити початкове зображення
+};
 
+// Обробник події для кнопки синього кольору
 blue.onclick = function () {
-  color='&chco=002c60';
+  color = '&chco=002c60'; // Встановити змінну кольору на синій
 };
+
+// Обробник події для кнопки небесно-блакитного кольору
 sky.onclick = function () {
-  color='&chco=23B1DE';
+  color = '&chco=23B1DE'; // Встановити змінну кольору на небесно-блакитний
 };
+
+// Обробник події для кнопки фіолетового кольору
 purple.onclick = function () {
-  color='&chco=660066';
+  color = '&chco=660066'; // Встановити змінну кольору на фіолетовий
 };
+
+// Обробник події для кнопки зеленого кольору
 green.onclick = function () {
-  color='&chco=336600';
+  color = '&chco=336600'; // Встановити змінну кольору на зелений
 };
 
-
-
+// Обробник події для кнопки генерації QR-коду
 buttongen.onclick = function () {
-	img.src = 'https://chart.googleapis.com/chart?cht=qr&chl='+text.value+'&chs=220x220&choe=UTF-8&chld=L|2'+color+'';
+  img.src = 'https://chart.googleapis.com/chart?cht=qr&chl=' + text.value + '&chs=220x220&choe=UTF-8&chld=L|2' + color;
 };
+
+// Обробник події для кнопки завантаження QR-коду
 buttonload.onclick = function () {
-  img.src = 'https://chart.googleapis.com/chart?cht=qr&chl='+text.value+'&chs='+size.value+'x'+size.value+'&choe=UTF-8&chld=L|2'+color+'';
+  img.src = 'https://chart.googleapis.com/chart?cht=qr&chl=' + text.value + '&chs=' + size.value + 'x' + size.value + '&choe=UTF-8&chld=L|2' + color;
   let imageUrl = img.src;
-  
+
   let link = document.createElement('a');
   link.href = imageUrl;
   link.target = '_blank';
